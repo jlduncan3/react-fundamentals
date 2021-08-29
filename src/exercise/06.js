648a8ae4bf52d5fd99a524b35f6538a022cacfc9
John Duncan <jduncan@smatechnologies.com>
2021-08-28 20:24:18 -0500

    finished react fundamentals

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,34 +4,54 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const [userName, setUserName] = React.useState('')
+  const [errorMessage, setErrorMessage] = React.useState('')
+  let submitDisabled = true
+  const userNameInputRef = React.useRef(null)
 
-  const [userName, setUserName] = React.useState('');
-
-  const handleChange = (e) => {
-    const value = e.target.value.toLowerCase();
-       setUserName(value);
-   
+  const handleChange = e => {
+    const value = e.target.value.toLower
+    const isValid = value === value.toLowerCase()
+    if (!isValid) {
+      setErrorMessage(isValid ? '' : 'Username must be lower case')
+      submitDisabled = true
+    } else {
+      setUserName(e.target.value)
+      setErrorMessage('')
+      submitDisabled = false
+    }
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmitUsername(userName);
+  const handleSubmit = e => {
+    e.preventDefault()
+    onSubmitUsername(userName)
   }
   //
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="userNameInput">Username:</label>
-        <input onChange={handleChange} name="userNameInput" type="text" value={userName}/>
+        <input
+          onChange={handleChange}
+          name="userNameInput"
+          type="text"
+          value={userName}
+        />
+        <label>{errorMessage ? errorMessage : ''}</label>
       </div>
-      <button type="submit" onSubmit={handleSubmit}>Submit</button>
+      <button
+        type="submit"
+        disabled={errorMessage !== ''}
+        onSubmit={handleSubmit}
+      >
+        Submit
+      </button>
     </form>
   )
 }
 
 function App() {
   const onSubmitUsername = username => alert(`You entered: ${username}`)
-  
 
   return <UsernameForm onSubmitUsername={onSubmitUsername} />
 }
